adf103ac103f5f41673aaf7d63d59797241764b8
volodya0 <volodauzuk02@gmail.com>
2023-12-11 01:21:07 +0200

    Refactor broker.js to use ES modules syntax for imports

--- broker.js
+++ broker.js
@@ -1,6 +1,11 @@
-const aedes = require("aedes")();
-const mqttServer = require("net").createServer(aedes.handle);
-require("dotenv").config();
+import Aedes from "aedes";
+import net from "net";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const aedes = Aedes();
+const mqttServer = net.createServer(aedes.handle);
 
 mqttServer.listen(process.env.LOCAL_PORT, function () {
     console.log(`Broker started on port: ${process.env.LOCAL_PORT}`);
