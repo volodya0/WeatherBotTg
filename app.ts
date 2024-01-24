6d5809cc47b6510b9fcd6960641e4b6f48e0243f
volodya0 <74143830+volodya0@users.noreply.github.com>
2024-01-24 12:46:22 +0000

    Temporary disabled OpenAi requests, added services to display info and changing the device

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 import mqtt from "mqtt";
-import { Telegraf, Context } from "telegraf";
+import { Telegraf, Context, Markup } from "telegraf";
 import { WeatherRecord } from "./models";
 import { OpenAI } from "openai";
 import * as dotenv from "dotenv";
@@ -13,14 +13,14 @@ const mqttClient = mqtt.connect(process.env.MQTT_BROKER_URL!, {
     password: process.env.MQTT_PASSWORD
 });
 
-mqttClient.on("error", (error) => console.log("Connection error:", error));
-mqttClient.on("reconnect", () => console.log("Client reconnecting..."));
-mqttClient.on("offline", () => console.log("Client is offline"));
+mqttClient.on("error", (error) => console.log("MQTT connection error:", error));
+mqttClient.on("reconnect", () => console.log("MQTT client reconnecting..."));
+mqttClient.on("offline", () => console.log("MQTT client is offline"));
 mqttClient.on("connect", function () {
-    console.log("Client connected to broker");
-    mqttClient.subscribe("measurements", function (err) {
+    console.log("MQTT client connected to broker");
+    mqttClient.subscribe("measurements/WeatherTgBot", function (err) {
         if (!err) {
-            mqttClient.publish("measurements", "Hello from node");
+            mqttClient.publish("measurements/WeatherTgBot", "Hello from node");
         } else {
             console.log("ERROR CONNECTING CLIENT");
             console.log(err);
@@ -29,59 +29,52 @@ mqttClient.on("connect", function () {
 });
 
 mqttClient.on("message", async function (topic, message) {
-    // Assume the message is a JSON string with weather data
     try {
-        const msg = message.toString();
+        const msgString = message.toString();
 
         console.log(
-            `Client received a message, topic="${topic}"  message="${msg}"`
+            `MQTT client received a message, topic="${topic}"  message="${msgString}"`
         );
 
-        let weatherRecord;
+        let msgParsed;
         try {
-            weatherRecord = JSON.parse(msg) as WeatherRecord;
-            if (
-                !(weatherRecord instanceof Object) ||
-                !weatherRecord.humidity ||
-                !weatherRecord.pressure ||
-                !weatherRecord.temperature
-            ) {
-                throw new Error();
-            }
+            msgParsed = JSON.parse(msgString);
         } catch {
-            console.log(`Failed to parse record, message="${msg}"`);
+            console.log(`Failed to parse, invalid JSON data, message="${msgString}"`);
             return;
         }
 
-        weatherHistory.addRecord(weatherRecord);
 
-        const notification = await requestWeatherNotification();
+        if (
+            msgParsed.humidity !== undefined &&
+            msgParsed.pressure !== undefined &&
+            msgParsed.temperature !== undefined 
+        ) {
+            handleReceivedMeasurenents(msgString, msgParsed as  WeatherRecord)
+        } else if (msgParsed.list_devices && Array.isArray(msgParsed.list_devices)){
+            handleReceivedDevices(msgParsed.list_devices as string[]);
+        }else if (msgParsed.selected_device){
+            handleReceivedInfo(msgParsed)
+        }else {
+            console.error("Cannot recognise message content");
+        }
 
-        console.log(
-            `Send notification to users, content="${notification}"  userIDs="${userIDs}"`
-        );
-
-        // Send notification to all subscribed users
-        userIDs.forEach((userId) => {
-            bot.telegram.sendMessage(
-                userId,
-                notification ?? "Error during generating a notification content"
-            );
-        });
-
-        store();
+       
     } catch (e) {
-        console.error("Error parsing MQTT message:", e);
+        console.error("Error during handling MQTT message:", e);
     }
 });
 
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY! });
+//const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY! });
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN!);
 
 // Array to store user IDs
-let userIDs: number[] = [];
+const userIDs: number[] = [];
+const usersRequestedDevideList: number[] = [];
+const usersRequestedInfo: number[] = [];
 
 bot.start((ctx: Context) => {
     ctx.reply("Привіт, тепер ти будеш отримуати опис змін показників!");
@@ -100,7 +93,105 @@ bot.start((ctx: Context) => {
 
 bot.help((ctx: Context) => ctx.reply("Send me a sticker"));
 
+bot.command("info", (ctx) => {
+    usersRequestedInfo.push(ctx.from.id);
+    mqttClient.publish("measurements/RequestSetting", `{
+        "sendler": "TgBot",
+        "requestCommand": "information"
+    }`);
+});
+
+bot.command("list", (ctx) => {
+    usersRequestedDevideList.push(ctx.from.id);
+    mqttClient.publish("measurements/RequestSetting", `{
+        "sendler": "TgBot",
+        "requestCommand": "listDevices"
+    }`);
+});
+
+bot.action(/^choose_device_(.*)$/, (ctx) => {
+    const selectedDevice = ctx.match[1]; 
+    // const userId = ctx.from!.id;
+
+    mqttClient.publish("measurements/RequestSetting", `{
+        "sendler": "TgBot",
+        "requestCommand": "changeDevice",
+        "data": "${selectedDevice}",
+    }`);
+
+    // Answer the callback query (optional)
+    ctx.answerCbQuery(`You selected: ${selectedDevice}, wait for updates`);
+});
+
 bot.launch();
+
+function handleReceivedMeasurenents(msg: string, record: WeatherRecord){
+    weatherHistory.addRecord(record);
+
+     const notification = msg;
+     console.log(
+         `Send notification to telegram bot users, content="${notification}"  userIDs="${userIDs}"`
+     );
+
+     userIDs.forEach((userId) => {
+         bot.telegram.sendMessage(
+             userId,
+             notification ?? "Error during generating a notification content"
+         );
+     });
+
+    store();
+};
+
+function handleReceivedDevices(list_devices: string[]) {
+    const userId = usersRequestedDevideList.shift();
+    if(!userId){
+        return;
+    }
+
+    console.log(
+        `Send device list to telegram bot user, devices="${list_devices}"  userID="${userId}"`
+    );
+
+    if (list_devices.length === 0) {
+        bot.telegram.sendMessage(
+            userId,
+            "Device liust is empty"
+        );
+        return;
+    }
+
+    const buttonLabels = list_devices.map((device) => {
+        return Markup.button.callback(device, `choose_device_${device}`);
+    });
+
+    const keyboard = Markup.inlineKeyboard(buttonLabels, { columns: 1 });
+
+    bot.telegram.sendMessage(
+        userId,
+        "Choose a device:",
+        keyboard
+    );
+}
+
+function handleReceivedInfo(info: Record<string, string>) {
+    const userId = usersRequestedInfo.shift();
+    if(!userId){
+        return;
+    }
+
+    const message = `Info:\n\tSelected Device: ${info.selected_device}\n\tAbsolute Pressure: ${info.absolut_pressure} hPa\n\tAltitude: ${info.altitude} meters\n\tRSSI: ${info.rssi}\n\tTimestep: ${info.timestep}\n\tStatus: ${info.status}`;
+
+    console.log(
+        `Send info list to telegram bot user, message="${message}"  userID="${userId}"`
+    );
+
+    bot.telegram.sendMessage(
+        userId,
+        message,
+    );
+}
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -108,14 +199,15 @@ async function requestWeatherNotification(): Promise<string | undefined> {
     const prompt = createOpenAIPrompt();
 
     try {
-        const completion = await openai.chat.completions.create({
+        /*const completion = await openai.chat.completions.create({
             max_tokens: 1000,
             messages: [{ role: "system", content: prompt }],
             model: "gpt-3.5-turbo",
             temperature: 0.6,
-        });
+        });*/
 
-        const message = completion.choices[0].message.content;
+        //const message = completion.choices[0].message.content;
+        const message = "";
         return message ?? undefined;
     } catch (error) {
         console.error("Error calling OpenAI API:", error);
